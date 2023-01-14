210ed01d402b5f1fd4b94eb8feb102f7de76978f
Maciej Minorowski <minorus@o2.pl>
2023-01-14 11:13:52 +0100

    1 commit

---
+++ src/main/java/org/example/library/Main.java
@@ -0,0 +1,9 @@
+package org.example.library;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String testCheckstyle = "test";
+        System.out.println(testCheckstyle);
+    }
+}
